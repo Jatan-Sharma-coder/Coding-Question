029442de33fb0a4b70823f2942d41dbf9117bacb
Jatan-Sharma-Code <JatanSharma475@gmail.com>
2025-10-30 13:24:49 +0530

    Committing Factorial Program

---
+++ Coding Question/Factorial.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coding_Question
+{
+    public class Factorial
+    {
+        public int CalculateFactRecursive(int input)
+        {
+            if(input == 1)
+            {
+                return 1;
+            }
+            return input * CalculateFactRecursive(input - 1);   
+        }
+        public int CalculateFactSimple(int input)
+        {
+            int result = 1;
+            for(int i = 1;i <= input; i++)
+            {
+                result *= i;        
+            }
+            return result;  
+        }
+    }
+}
---
+++ Coding Question/Program.cs
@@ -0,0 +1,26 @@
+﻿using Coding_Question;
+using System;
+using System.Runtime.InteropServices;
+using System.Transactions;
+
+namespace StartingPoint
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Factoiral of Number 
+            Factorial factorial = new Factorial();
+            Console.WriteLine("Enter any number to calulate Factorials");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            //Simple
+            int fact = factorial.CalculateFactRecursive(input);
+            Console.WriteLine($"Factorial of {input} is {fact}");
+
+            //Recursive
+            fact = factorial.CalculateFactSimple(input);
+            Console.WriteLine($"Factorial of {input} is {fact}");
+        }
+    }
+}
